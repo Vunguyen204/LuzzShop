48bcf1ddc2258b456903fee6853b28833bde2687
Vunguyen <nhuvu0011@gmail.com>
2026-06-10 23:59:09 +0700

    feat: add scroll to top functionality

---
+++ Frontend/src/components/scrollToTopButton/index.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import "./style.scss";
+
+function ScrollTopButton() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisible = () => {
+      setVisible(window.scrollY > 300);
+    };
+
+    window.addEventListener("scroll", toggleVisible);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    const scrollStep = -window.scrollY / 50;
+
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 10);
+  };
+
+  return (
+    visible && (
+      <button className="scroll-top-btn" onClick={scrollToTop}>
+        <FontAwesomeIcon icon={faArrowUp} />
+      </button>
+    )
+  );
+}
+
+export default ScrollTopButton;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import ScrollToTop from "./components/scrollToTop/index.jsx";
+import ScrollTopButton from "./components/scrollToTopButton/index.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
+      <ScrollTopButton />
     </BrowserRouter>
   </React.StrictMode>
 );
